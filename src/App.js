9e0326d79f1f6951d1cbbf9b186d6820f5b47b69
dth <dth@dthlabs.com>
2020-05-20 21:35:39 +0200

    adding navbar component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
       <>
       
       {/* container */}
-      <div className="container-fluid">
+      <div className="container-fluid w-header">
 
         {/* navbar */}
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -31,8 +31,8 @@ class App extends React.Component {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav mr-auto">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarColor01">
+            <ul className="navbar-nav">
               <li className="nav-item active">
                 <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
               </li>
@@ -72,7 +72,8 @@ class App extends React.Component {
       
 
       {/* container */}
-      <div className="container">
+      <div className="container home">
+        {/* homepage */}
         <div className="row">
           <div className="col-sm-4">
             <h3>Column 1</h3>
@@ -85,11 +86,49 @@ class App extends React.Component {
             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
           </div>
           <div className="col-sm-4">
-            <h3>Column 3</h3>        
+            <h3>Column P</h3>        
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
           </div>
         </div>
+
+        <div className="row">
+          <div className="col-sm-8">
+            <h3>Column A</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+            <div className="row">
+              <div className="col-sm-12">
+                <h3>Row 1</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+              <div className="col-sm-12">
+                <h3>Row 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+              <div className="col-sm-12">
+                <h3>Row 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column P</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+          </div>
+        </div>
+
+        <div className="row">
+              <div className="col-sm-12">
+                <p>0 results found.</p>
+              </div>
+        </div>
+
+
       </div>
       {/* /container */}
 
