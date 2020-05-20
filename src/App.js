192c27200bb811eded8cb1fc0c4fb6f5a4f52612
dth <dth@dthlabs.com>
2020-05-20 22:24:34 +0200

    REST call to the backend API

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 /* import logo from './logo.svg'; */
 import './App.css';
 import Navbar from './components/navbar';
-import Search from './components/searchbar';
 import Searchbar from './components/searchbar';
 
 class App extends React.Component {
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,6 +1,45 @@
 import React from 'react';
 
 class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      scrolled: false,
+      title: '', 
+      country: '', 
+      wines: [] 
+    };
+
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeCountry = this.handleChangeCountry.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  
+  }
+
+  handleChangeTitle(event) {
+    this.setState({title: event.target.value});
+  }
+  
+  handleChangeCountry(event) {
+    this.setState({country: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    /*
+    http://192.168.1.44:3000/wines?filter={"where":{"and":{"title":{"like":"bru%25","options":"i"}},{"country":{"like":"bra%25","options":"i"}}},"limit":3}
+    http://192.168.1.44:3000/wines?filter[where][title][like]=Bru%25&filter[where][country][like]=US%25&filter[limit]=3
+    */
+    // fetch('http://192.168.1.44:3000/wines?filter={"where":{"title":{"like":"'+ this.state.title + '%25","options":"i"}},"limit":3}')
+    fetch('http://192.168.1.44:3000/wines?filter[where][title][like]=%25'+ this.state.title + '%25&filter[where][country][like]='+ this.state.country + '%25&filter[limit]=30')
+    .then(res => res.json())
+    .then((data) => {
+        this.setState({ wines: data })
+    })
+    .catch(console.log)
+        
+  }
+  
   
   render() {
 
@@ -9,10 +48,10 @@ class Searchbar extends React.Component {
       <div className="jumbotron text-center">
         <p>Search for your favorite wine</p> 
         <div className="container">
-          <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2 sinput-title" type="text" placeholder="Search" />
-              <input className="form-control mr-sm-2 sinput-country" type="text" placeholder="Country" />
-              <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+          <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
+              <input className="form-control mr-sm-2 sinput-title" type="text" defaultValue={this.state.title} onChange={this.handleChangeTitle} placeholder="Search for a wine" />
+              <input className="form-control mr-sm-2 sinput-country" type="text" defaultValue={this.state.country} onChange={this.handleChangeCountry} placeholder="Country" />
+              <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit" >Search</button>
             </form>
           </div>
       </div>
